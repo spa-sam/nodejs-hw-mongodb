21bd20653b68609982b49113e1b89e505865beea
Pablo <st.spasam@gmail.com>
2024-07-02 21:05:44 +0300

    fix

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import createError from 'http-errors';
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    next(createError(400, 'Invalid contact ID'));
+    next(createError(404, 'Not found'));
   }
   next();
 };
