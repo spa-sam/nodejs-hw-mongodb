1013570b9a96659592b8100ab994b4d12e40265a
Pablo <st.spasam@gmail.com>
2024-07-20 20:14:28 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import setupServer from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 async function startApp() {
   try {
     await initMongoConnection();
+    await createDirIfNotExists(TEMP_UPLOAD_DIR);
+    await createDirIfNotExists(UPLOAD_DIR);
     setupServer();
   } catch (error) {
     console.error('Error starting the application:', error);
